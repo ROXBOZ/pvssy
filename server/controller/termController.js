31caf22943e13b8d80442bc0bde6fcaf96cfc978
ROXBOZ <hello@roxanne-borloz.net>
2023-02-15 23:41:08 +0100

    played around to link terms to pains

--- server/controller/termController.js
+++ server/controller/termController.js
@@ -15,4 +15,23 @@ const getAllTerms = async (req, res) => {
   }
 };
 
-export { getAllTerms };
+// getTermsByPain
+const getTermsByName = async (req, res) => {
+  try {
+    const requestedTerm = await termModel.find({ term: req.query.term }).exec();
+    if (requestedTerm.length === 0) {
+      res.status(200).json({
+        msg: "Pas de terme correspondants à la recherche",
+      });
+    } else {
+      res.status(200).json({ requestedTerm });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+      msg: "Problème serveur",
+    });
+  }
+};
+
+export { getAllTerms, getTermsByName };
