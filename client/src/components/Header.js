88c55c6c0e8252527969db6775071ac2c1d1fe18
ROXBOZ <hello@roxanne-borloz.net>
2023-03-01 12:30:21 +0100

    worked on the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,62 +16,61 @@ const Header = () => {
 
   return (
     <header>
-      <button
-        className="burger-menu"
-        aria-label="Menu"
-        onClick={() => openMenu()}
-      >
-        <span id="nav-icon" className={!isOpen ? "close" : "open"}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-        <span>menu</span>
-      </button>
       <Link to="/" className="logo">
         pvssy talk
       </Link>
+      <div className="menu-container">
+        <button className="burger-menu" onClick={() => openMenu()}>
+          <span id="nav-icon" className={!isOpen ? "close" : "open"}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+          <span className="menu-label">menu</span>
+        </button>
 
-      <nav className={isOpen ? "vertical" : "horizontal"}>
-        <ul>
-          <li>
-            <NavLink to="a-propos">À propos</NavLink>
-          </li>
-          <li>
-            <NavLink to="gerer-soi-meme">Gérer soi-même</NavLink>
-          </li>
-          <li>
-            <NavLink to="trouver-de-l-aide">Trouver de l’aide</NavLink>
-          </li>
-          <li>
-            <NavLink to="shop">Shop</NavLink>
-          </li>
-          <li>
-            <NavLink to="agenda">Agenda</NavLink>
-          </li>
-          <li>
-            {userProfile ? (
-              <div className="logout-header">
-                <Link
-                  className="user-icon-link"
-                  to="/profile/ajouter"
-                  aria-label="profil"
-                >
-                  <FontAwesomeIcon
-                    className="user-icon"
-                    id="userIcon"
-                    icon={faUser}
-                  />
-                </Link>
-                <button onClick={logout}>se déconnecter</button>
-              </div>
-            ) : (
-              <Link to="/login">se connecter</Link>
-            )}
-          </li>
-        </ul>
-      </nav>
+        <nav className={isOpen ? "vertical" : "horizontal"}>
+          <ul>
+            <li>
+              <NavLink to="a-propos">À propos</NavLink>
+            </li>
+            <li>
+              <NavLink to="gerer-soi-meme">Gérer soi-même</NavLink>
+            </li>
+            <li>
+              <NavLink to="trouver-de-l-aide">Trouver de l’aide</NavLink>
+            </li>
+            <li>
+              <NavLink to="shop">Shop</NavLink>
+            </li>
+            <li>
+              <NavLink to="agenda">Agenda</NavLink>
+            </li>
+            <li>
+              {userProfile ? (
+                <div className="logout-header">
+                  <Link
+                    className="user-icon-link"
+                    to="/profile/ajouter"
+                    aria-label="profil"
+                  >
+                    <FontAwesomeIcon
+                      className="user-icon"
+                      id="userIcon"
+                      icon={faUser}
+                    />
+                    <p>{userProfile.userName}</p>
+                  </Link>
+                  <button onClick={logout}>se déconnecter</button>
+                </div>
+              ) : (
+                <Link to="/login">se connecter</Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Breadcrumbs />
     </header>
   );
