8301e9136a578118b98b625b563235624d298ea3
ROXBOZ <hello@roxanne-borloz.net>
2023-03-29 15:42:29 +0200

    cors options including array of allowed urls

--- server/index.js
+++ server/index.js
@@ -39,11 +39,24 @@ const addMiddlewares = () => {
     })
   );
 
-  const corsOptions = {
-    origin: "*",
-    credentials: true,
+  // const corsOptions = {
+  //   origin: "*",
+  //   credentials: true,
+  // };
+  const allowedUrls = [
+    "http://https://pvssy-frontend.vercel.app",
+    "http://localhost:3000",
+  ];
+  const corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowedUrls.indexOf(req.header("Origin")) !== -1) {
+      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
   };
-  app.use(cors(corsOptions));
+  app.use(cors(corsOptionsDelegate));
   cloudinaryConfig();
   app.use(passport.initialize());
   passport.use(jwtStrategy);
