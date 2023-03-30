3c71babbf7f3d2fd22f75efa900b6492c4e317b2
ROXBOZ <hello@roxanne-borloz.net>
2023-03-30 10:38:15 +0200

    cors options with success status

--- server/index.js
+++ server/index.js
@@ -44,19 +44,23 @@ const addMiddlewares = () => {
     "http://localhost:3000",
   ];
 
-  // const corsOptionsDelegate = function (req, callback) {
-  //   var corsOptions;
-  //   if (allowedUrls.indexOf(req.header("Origin")) !== -1) {
-  //     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  //   } else {
-  //     corsOptions = { origin: false }; // disable CORS for this request
-  //   }
-  //   callback(null, corsOptions); // callback expects two parameters: error and options
-  // };
+  const corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowedUrls.indexOf(req.header("Origin")) !== -1) {
+      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+  };
 
+  const corsOptions2 = {
+    origin: "https://pvssy-frontend.vercel.app",
+    optionsSuccessStatus: 200,
+  };
   // app.use(cors({ origin: allowedUrls }));
-  app.use(cors());
   // app.use(cors(corsOptionsDelegate));
+  app.use(cors(corsOptions2));
   cloudinaryConfig();
   app.use(passport.initialize());
   passport.use(jwtStrategy);
@@ -77,38 +81,17 @@ const startServer = () => {
   });
 };
 
-// async function controller() {
-//   addMiddlewares(); // always add middlewares before loading routes
-//   loadRoutes();
-
-//   // PUT THE MONGODBCONNEXION IN TRY AND CATCH (4)
-//   try {
-//     await mongoDBConnexion();
-//   } catch (error) {
-//     console.log("error :", error);
-//   }
-
-//   startServer();
-// }
-// controller();
-
-function middlewares() {
-  addMiddlewares();
-  console.log("middlewaresLoaded :");
+async function controller() {
+  addMiddlewares(); // always add middlewares before loading routes
   loadRoutes();
-  console.log("routes loaded");
-  conntectMongoDB();
-}
 
-async function conntectMongoDB(params) {
-  console.log("connectiong to mongo");
+  // PUT THE MONGODBCONNEXION IN TRY AND CATCH (4)
   try {
     await mongoDBConnexion();
-    console.log("mongo conected");
-    startServer();
   } catch (error) {
     console.log("error :", error);
   }
-}
 
-middlewares();
+  startServer();
+}
+controller();
