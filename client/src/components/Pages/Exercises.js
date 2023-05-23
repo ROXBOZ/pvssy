cb0042a1fd3b7b559c59b0c17439f1602e285097
ROXBOZ <hello@roxanne-borloz.net>
2023-05-23 12:45:33 +0200

    minor fixes

--- client/src/components/Pages/Exercises.js
+++ client/src/components/Pages/Exercises.js
@@ -36,9 +36,32 @@ const Exercises = () => {
       <HeadingArea
         title="Exercices"
         level="h1"
-        subtitle="gnagnagna et gnagnagna"
+        subtitle="Les exercices proposés ont pour but de t’aider à reconnecter avec ton
+        corps et de redécouvrir ton anatomie et ton plaisir."
       />
+      <div className="grid-area">
+        <div className="centered">
+          <p
+            style={{
+              paddingBottom: "2rem",
+              borderBottom: "1px solid #ce96ff",
+              marginBottom: "2rem",
+            }}
+          >
+            Certains se font solo, d’autres à deux. Il n’y a pas d’enjeu de
+            réussite ni de performance. L’échec n’existe pas car tout est
+            possible dans l’intimité, et la pénétration et l’orgasme sont
+            optionnels. Si tu trouves ça « bizarre, pas naturel » de faire ces
+            exercices, c’est normal, car on ne fait jamais ça et personne ne
+            nous l’apprend. Si tu as des pensées critiques, laisse-les passer et
+            accorde-toi le droit d’essayer, même plusieurs fois. Dès que tu as
+            mal, arrête. Reste à l’écoute de tes sensations, tes besoin, tes
+            envies et tes limites.
+          </p>
+        </div>
+      </div>
       <CreateTags tags={painList} />
+
       <div className="exercises-container">
         {filteredData &&
           filteredData.map((ex) => (
